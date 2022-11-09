5d6a318c0507616baea5b6633f0d7bf3ea415c8d
Randima Dilshani <randimadilshani98@gmail.com>
2022-11-09 21:17:14 +0530

    Get Blogs

--- App.js
+++ App.js
@@ -32,42 +32,32 @@ export default function App() {
   return (
     <NavigationContainer>
 
-      <Stack.Navigator initialRouteName="AllOrganizations">
-        <Stack.Screen name="BlogsList" component={BlogsList} />
+        <Stack.Navigator initialRouteName="LoadingPage">
+      {/* <Stack.Navigator initialRouteName="AllOrganizations"> */}
+        {/* <Stack.Screen name="BlogsList" component={BlogsList} />
         <Stack.Screen name="UpdateList" component={UpdateDeleteList} />
-        <Stack.Screen name="UpdateBlogs" component={UpdateBlogs} />
+        <Stack.Screen name="UpdateBlogs" component={UpdateBlogs} /> */}
         <Stack.Screen name="AddOrganization" component={AddOrganization} />
         <Stack.Screen name="AllOrganizations" component={AllOrganizations} />
         <Stack.Screen name="EditOrganizaton" component={EditOrganizaton} />
         <Stack.Screen name="Organizations" component={Organizations} />
         <Stack.Screen name="AllEvents" component={AllEvents} />
-        <Stack.Screen
-          name="SpecificEventAdmin"
-          component={SpecificEventAdmin}
-        />
+        <Stack.Screen name="SpecificEventAdmin" component={SpecificEventAdmin}/>
         <Stack.Screen name="SpecificEventUser" component={SpecificEventUser} />
-        <Stack.Screen
-          name="SpecificOrganization"
-          component={SpecificOrganization}
-        />
+        <Stack.Screen name="SpecificOrganization" component={SpecificOrganization} />
         <Stack.Screen name="Members" component={Members} />
-
-      <Stack.Navigator initialRouteName="LoadingPage">
         <Stack.Screen name="LoadingPage" component={LoadingScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="SignInScreen" component={SignInScreen} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
         <Stack.Screen name="BlogsList" component={BlogsList} />
-      <Stack.Screen name="UpdateList" component={UpdateDeleteList} />
-      <Stack.Screen name="UpdateBlogs" component={UpdateBlogs} />
-      <Stack.Screen name="DeleteBlogs" component={DeleteBlogs} />
-<<<<<<< HEAD
+        <Stack.Screen name="UpdateList" component={UpdateDeleteList} />
+        <Stack.Screen name="UpdateBlogs" component={UpdateBlogs} />
+        <Stack.Screen name="DeleteBlogs" component={DeleteBlogs} />
       <Stack.Screen name="AddBlog" component={AddBlog} />
       <Stack.Screen name="AdditionalVideosImages" component={AdditionalVideosImages} />
-=======
 
->>>>>>> 0a5e51d13958f713c17ee9445730a4e215746545
       </Stack.Navigator>
     </NavigationContainer>
   );
